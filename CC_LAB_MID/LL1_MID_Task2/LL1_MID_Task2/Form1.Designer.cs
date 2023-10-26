177eb6fbcd7ab98d6a4a3f4b3f263cb1abf6ef54
Zia Ur Rahman (Zia) <113768789+Zia-Ur-Rahman-082@users.noreply.github.com>
2023-10-26 15:53:31 +0500

    Add files via upload

---
+++ CC_LAB_MID/LL1_MID_Task2/LL1_MID_Task2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LL1_MID_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.InputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.Location = new System.Drawing.Point(167, 195);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(139, 37);
+            this.ParseButton.TabIndex = 0;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = true;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // InputRichTextBox
+            // 
+            this.InputRichTextBox.Location = new System.Drawing.Point(116, 57);
+            this.InputRichTextBox.Name = "InputRichTextBox";
+            this.InputRichTextBox.Size = new System.Drawing.Size(242, 96);
+            this.InputRichTextBox.TabIndex = 1;
+            this.InputRichTextBox.Text = "";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(207, 259);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(51, 20);
+            this.ResultLabel.TabIndex = 2;
+            this.ResultLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(517, 318);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.InputRichTextBox);
+            this.Controls.Add(this.ParseButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ParseButton;
+        private System.Windows.Forms.RichTextBox InputRichTextBox;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ CC_LAB_MID/PasswordGenerator/PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.GeneratedPasswordLabel = new System.Windows.Forms.Label();
+            this.RegistrationNumberTextBox = new System.Windows.Forms.TextBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Reg Number:";
+            // 
+            // GeneratedPasswordLabel
+            // 
+            this.GeneratedPasswordLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.GeneratedPasswordLabel.AutoSize = true;
+            this.GeneratedPasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GeneratedPasswordLabel.Location = new System.Drawing.Point(58, 315);
+            this.GeneratedPasswordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GeneratedPasswordLabel.Name = "GeneratedPasswordLabel";
+            this.GeneratedPasswordLabel.Padding = new System.Windows.Forms.Padding(3);
+            this.GeneratedPasswordLabel.Size = new System.Drawing.Size(433, 39);
+            this.GeneratedPasswordLabel.TabIndex = 1;
+            this.GeneratedPasswordLabel.Text = "Your Password will appear here";
+            // 
+            // RegistrationNumberTextBox
+            // 
+            this.RegistrationNumberTextBox.Location = new System.Drawing.Point(64, 53);
+            this.RegistrationNumberTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.RegistrationNumberTextBox.Name = "RegistrationNumberTextBox";
+            this.RegistrationNumberTextBox.Size = new System.Drawing.Size(401, 26);
+            this.RegistrationNumberTextBox.TabIndex = 2;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(174, 240);
+            this.GenerateButton.Margin = new System.Windows.Forms.Padding(2);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(159, 60);
+            this.GenerateButton.TabIndex = 3;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(64, 127);
+            this.FirstNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(401, 26);
+            this.FirstNameTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter First Name";
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(64, 197);
+            this.LastNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(401, 26);
+            this.LastNameTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 175);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(518, 400);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.RegistrationNumberTextBox);
+            this.Controls.Add(this.GeneratedPasswordLabel);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label GeneratedPasswordLabel;
+        private System.Windows.Forms.TextBox RegistrationNumberTextBox;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
